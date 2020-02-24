3a61261d7c0fdf868075728add9f161638987275
Kenny Gray <kenneth-gray@users.noreply.github.com>
2020-02-24 08:43:03 +0000

    fix: Added initialiseSsr function so that when using SSR the DOM will match client hydration
    fix #7

--- src/Accordion/index.ts
+++ src/Accordion/index.ts
@@ -1,4 +1,4 @@
-export { default } from './Accordion';
+export { default, initialiseForSsr } from './Accordion';
 export { default as Section } from './Section';
 export { default as Header } from './Header';
 export { default as Panel } from './Panel';
